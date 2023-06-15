f01501b3378fd6f86c5e317f0d63f69017055eb0
Vanessa <vanemp21@gmail.com>
2023-06-15 21:11:58 +0200

    comienzo seccion 1

---
+++ src/JournalApp.jsx
@@ -0,0 +1,9 @@
+
+
+export const JournalApp = () => {
+  return (
+  <>
+  <h1>Hola mundo</h1>
+  </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import './styles.css'
+import { JournalApp } from './JournalApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <JournalApp />
   </React.StrictMode>,
 )
